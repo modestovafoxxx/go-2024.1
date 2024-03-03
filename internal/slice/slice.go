efebfc8a7902b96b516adab5ace2ce394364957f
Antony Pavlenko <antonpawlenko@yandex.ru>
2024-03-03 15:17:06 +0300

    hw-1

---
+++ internal/slice/slice.go
@@ -0,0 +1,19 @@
+package slice
+
+// AppendElement добавляет элемент в конец слайса.
+func AppendElement(ints []int, elem int) []int {
+	// Код тут
+	return ints
+}
+
+// RemoveElement удаляет последний элемент из слайса. Если массив пуст, функция возвращает оригинальный пустой массив.
+func RemoveElement(ints []int) []int {
+	// Код тут
+	return ints
+}
+
+// AddOneToAll увеличивает каждый элемент массива на единицу.
+func AddOneToAll(ints []int) []int {
+	// Код тут
+	return ints
+}
