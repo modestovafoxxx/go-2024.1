30170b6935f1004007d7271e60e824b29d66be60
modestovafoxxx <olmodesto1004@gmail.com>
2024-03-13 17:42:20 +0300

    all functions

--- internal/slice/slice.go
+++ internal/slice/slice.go
@@ -3,17 +3,26 @@ package slice
 // AppendElement добавляет элемент в конец слайса.
 func AppendElement(ints []int, elem int) []int {
 	// Код тут
+	ints = append(ints, elem)
 	return ints
 }
 
 // RemoveElement удаляет последний элемент из слайса. Если массив пуст, функция возвращает оригинальный пустой массив.
 func RemoveElement(ints []int) []int {
 	// Код тут
+	if len(ints) == 0 {
+		return ints
+	}
+	last_el := len(ints) - 1
+	ints = ints[:last_el]
 	return ints
 }
 
 // AddOneToAll увеличивает каждый элемент массива на единицу.
 func AddOneToAll(ints []int) []int {
 	// Код тут
+	for i := 0; i <= len(ints)-1; i++ {
+		ints[i]++
+	}
 	return ints
 }
